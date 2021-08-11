658ef1b730244ded7894ccae8c89766239437d64
Dominic Cupo <Dominic.Cupo@Xandr.com>
2021-08-11 11:13:51 -0400

    Linter fixes for health handler

--- internal/handlers/health.go
+++ internal/handlers/health.go
@@ -1,7 +1,12 @@
 package handlers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
-func Health(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("OK"))
+func Health(w http.ResponseWriter, _ *http.Request) {
+	if _, err := w.Write([]byte("OK")); err != nil {
+		log.Println("issue writing ping output: ", err.Error())
+	}
 }
